6a6cd0a61413ad7780f6a9b2cc70ce77ebc416f8
czw <543481852@qq.com>
2024-05-22 21:23:37 +0800

    加上协程和Retrofit依赖

--- app/src/main/java/com/example/testarchit/MainActivity.kt
+++ app/src/main/java/com/example/testarchit/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.testarchit
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.appcompat.app.AppCompatActivity
 import com.example.testarchit.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -54,6 +54,8 @@ class MainActivity : AppCompatActivity() {
             setupActionBarWithNavController(navController, appBarConfiguration)
             it.setupWithNavController(navController)
         }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
